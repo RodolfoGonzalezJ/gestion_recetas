83bd850bd5cad8981749657080acf90a88648456
Sergio De La Hoz <sddelahoz@unicesar.edu.co>
2025-03-26 17:12:41 -0500

    Refactor estructura de la aplicación y agregar clase de colores básicos

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:gestion_recetas/utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Manejo del Tema oscuro y claro
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      // Cambiamos el tema oscuro por el tema claro
+      darkTheme: TAppTheme.darkTheme,
+    );
+  }
+}
---
+++ lib/utils/constansts/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CColors {
+  CColors._();
+
+  // Colores de la aplicación basicos
+  static const Color primaryColor = Color.fromRGBO(25, 131, 48, 100);
+  static const Color secondaryColor = Color.fromRGBO(31, 175, 63, 100);
+  static const Color resaltar = Color.fromRGBO(30, 103, 46, 100);
+  // Colores de texto
+  static const Color primaryTextColor = Color.fromRGBO(0, 0, 0, 80);
+  static const Color secondaryTextColor = Color.fromRGBO(51, 51, 51, 80);
+  static const Color textBlanco = Color.fromRGBO(255, 255, 255, 100);
+  // Colores de fondo
+  static const Color light = Color.fromRGBO(255, 255, 255, 1);
+  static const Color dark = Color.fromRGBO(0, 0, 0, 100);
+  //Color Seleccionado
+  static const Color selected = Color.fromRGBO(0, 0, 0, 10);
+  static const Color selectedText = Color(0xFF027E1E);
+}
