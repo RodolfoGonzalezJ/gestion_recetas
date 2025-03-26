3cff9f5e16d5b600d50b598410c502fce4fd44b5
Sergio De La Hoz <sddelahoz@unicesar.edu.co>
2025-03-25 20:52:08 -0500

    Refactor main app structure and add custom light and dark themes

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:gestion_recetas/utils/theme/custom_themes/text_theme.dart';
+
+//Usamos la Letra T para refenciar a Theme
+//y clase
+class TAppTheme {
+  //Creamos un constructor privado
+  TAppTheme._();
+  //Creamos unass variables de tipo ThemeData
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Color.fromRGBO(25, 131, 48, 100),
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Color.fromRGBO(25, 131, 48, 100),
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
