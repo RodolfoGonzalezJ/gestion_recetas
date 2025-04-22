dfad7e2ed662778d0ff612fd6e9f20471e644a8a
RodolfoGonzalezJ <rgonzalezjimenez8@gmail.com>
2025-04-22 01:53:17 -0500

    arreglo del home

--- lib/features/home/screens/home.dart
+++ lib/features/home/screens/home.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_recetas/features/home/screens/widgets/floating_menu_button.dart';
+import 'package:gestion_recetas/utils/constants/colors.dart';
+import 'package:gestion_recetas/utils/helpers/helper_functions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  final currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final bool isDark = THelperFunctions.isDarkMode(context);
+    final Color backgroundColor = isDark ? CColors.dark : CColors.primaryColor;
+    final Color iconColor = Colors.white;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Gestión de Alimentos'),
         actions: [
+          IconButton(icon: Icon(Icons.face), onPressed: () {}),
           IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
         ],
       ),
@@ -44,15 +52,35 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Categorías'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Perfil',
-          ),
-        ],
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          labelTextStyle: WidgetStateProperty.resolveWith<TextStyle>((
+            Set<WidgetState> states,
+          ) {
+            return TextStyle(
+              color: isDark ? Colors.white : Colors.white,
+              fontSize: 16,
+            );
+          }),
+        ),
+        child: NavigationBar(
+          //selectedIndex: currentPageIndex,
+          backgroundColor: backgroundColor,
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home, color: iconColor),
+              label: 'Inicio',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.list, color: iconColor),
+              label: 'Inventario',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.favorite, color: iconColor),
+              label: 'Favoritos',
+            ),
+          ],
+        ),
       ),
       floatingActionButton: const FloatingMenuButton(),
     );
